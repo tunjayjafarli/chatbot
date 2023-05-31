de4102da0983aaaaf42e2aad5288797a48b11e47
Tunjay Jafarli <tunjay.jafarli@gmail.com>
2023-05-31 16:13:16 -0700

    update bard api version & token

--- bard_api.py
+++ bard_api.py
@@ -1,20 +1,21 @@
-import os
-import bardapi
+from bardapi import Bard
 
 from constants import GOOGLE_BARD_API_KEY
 
 def get_response_from_bard(question):
-    """Send an API request and get a response."""    
+    """
+    Send an API request and get a response.
+    """
     if question:
-        response = bardapi.core.Bard(timeout=10).get_answer(question)
-        
+        bard = Bard(token=GOOGLE_BARD_API_KEY)
+
+        response = bard.get_answer(question)
+
         return response['content']
 
 
 def main():
-    print('Running bard-api.py...')
-
-    os.environ['_BARD_API_KEY'] = GOOGLE_BARD_API_KEY
+    print('Running bard_api.py...')
 
     while True:
         input_text = input('> ')
